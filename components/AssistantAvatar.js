da7f120b4309a4d85a60da5752bd37a9f13beb0d
ntmr <dtntmr@gmail.com>
2025-05-21 00:12:09 +0300

    Update AssistantAvatar.js

--- components/AssistantAvatar.js
+++ components/AssistantAvatar.js
@@ -1,7 +1,8 @@
+// components/AssistantAvatar.js
 export default function AssistantAvatar() {
   return (
-    <div className="assistant-avatar">
-      <img src="/robot-avatar.png" alt="Ассистент" />
+    <div className="avatar">
+      <img src="/robot-avatar.png" alt="Assistant Avatar" width={100} />
     </div>
   );
 }
