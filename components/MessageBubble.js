217477f888630d24998bb06b07706b09b7140de6
ntmr <dtntmr@gmail.com>
2025-05-21 00:11:17 +0300

    Update MessageBubble.js

--- components/MessageBubble.js
+++ components/MessageBubble.js
@@ -1,8 +1,9 @@
-// components/AssistantAvatar.js
-export default function AssistantAvatar() {
+// components/MessageBubble.js
+export default function MessageBubble({ message }) {
+  const isUser = message.sender === 'user';
   return (
-    <div className="avatar">
-      <img src="/robot-face.png" alt="Assistant Avatar" width={100} />
+    <div className={`bubble-container ${isUser ? 'user' : 'assistant'}`}>
+      <div className="bubble">{message.text}</div>
     </div>
   );
 }
